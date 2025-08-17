7d7206541281251303a58496fb87b741e4a891e1
Adarsh11Sharma <20102169.adarsh.sharma@gmail.com>
2025-08-17 23:16:43 +0530

    .

--- PrintMsg.java
+++ PrintMsg.java
@@ -2,17 +2,8 @@ import java.util.*;
 
 public class PrintMsg{
 
+    public static boolean  PrintMsg(int arr[]){
 
-public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-     
-        int a;
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-         arr[i]=sc.nextInt();
-          
-         }
 
          boolean ascending =true;
          boolean decending=true;
@@ -39,6 +30,26 @@ public static void main(String[] args) {
                 System.out.println("not sorted");
             }
          }
+return true;
+    }
+
+
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+     
+       
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+         arr[i]=sc.nextInt();
+          
+         }
+
+
+         boolean ans=PrintMsg(arr);
+         System.out.println(ans);
+
+        
             
 
             
