cf0eba5b3d03eee546db86c099f22e7aa8532a6e
Adarsh11Sharma <20102169.adarsh.sharma@gmail.com>
2025-08-20 11:57:58 +0530

    1

--- Day 7/Palindrone.java
+++ Day 7/Palindrone.java
@@ -3,19 +3,36 @@ import  java.util.*;
 public class Palindrone{
 
  public static void palindrone(String str){
-    int j=0;
- char ch[]=str.toCharArray();
- for (int i=ch.length-1;i>0;i--){
-    ch[j++]=ch[i];
+//     int j=0;
+//  char ch[]=str.toCharArray();
+//  for (int i=ch.length-1;i>0;i--){
+//     ch[j++]=ch[i];
 
 
+// }
+// String ss=String.valueOf(ch);
+
+// if(ss.equals(str)){
+//     System.out.println("palindrone");
+// }else{
+//     System.out.println("not");
+// }
+
+char ch[]=str.toCharArray();
+int i=0,flag=0;
+int l=ch.length;
+int h=l/2;
+for( i=0;i<h;i++){
+if(ch[i]==ch[l-1-i]){
+    flag =1;
 }
-String ss=String.valueOf(ch);
+}
 
-if(ss.equals(str)){
-    System.out.println("palindrone");
-}else{
-    System.out.println("not");
+if(flag==1){
+    System.out.println("Palindrone");
+}
+else{
+    System.out.println("Not");
 }
 
 
