7dcfd0d7a0a2d93479400ce9a699f2192b715731
Adarsh11Sharma <20102169.adarsh.sharma@gmail.com>
2025-08-20 09:33:54 +0530

    .

---
+++ Day 6/TransposeMatrixIntoAnotherMatrix.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+
+public   class TransposeMatrixIntoAnotherMatrix {
+
+    public static void TransposeMatrixIntoAnotherMatrix(int arr[][]){
+        int [][]newarr=new int [3][3];
+
+
+for (int i = 0; i <3; i++) {
+    for (int j = 0; j < 3; j++) {
+        newarr[j][i]= arr[i][j];
+    }
+    
+}
+     for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(newarr[i][j]+" ");
+                
+            }
+            System.out.println(" ");
+            
+        }
+
+
+
+}
+
+    
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[][]=new int[n][n];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j]=sc.nextInt();
+                
+            }
+            
+        }
+
+        // printing the 2d array
+
+         for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j]+" ");
+                
+            }
+            System.out.println(" ");
+            
+        }
+
+        TransposeMatrixIntoAnotherMatrix(arr);
+
+
+    }
+}
