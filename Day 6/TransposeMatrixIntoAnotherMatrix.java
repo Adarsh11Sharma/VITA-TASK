f9912f1c8477aa54ec69c1c35d24f432f625030e
Adarsh11Sharma <20102169.adarsh.sharma@gmail.com>
2025-08-24 21:28:13 +0530

    .

--- Day 6/TransposeMatrixIntoAnotherMatrix.java
+++ Day 6/TransposeMatrixIntoAnotherMatrix.java
@@ -5,6 +5,7 @@ public   class TransposeMatrixIntoAnotherMatrix {
 
     public static void TransposeMatrixIntoAnotherMatrix(int arr[][]){
         int [][]newarr=new int [3][3];
+        int p;
 
 
 for (int i = 0; i <3; i++) {
